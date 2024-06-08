a34cd08440e2ebda12444168bf4f5c0bf828fc10
Tais Kotliar <kryos.eva@gmail.com>
2024-06-08 12:59:51 +0300

    up, down, left, right

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
+import random
+
 
 pygame.init()
 
@@ -18,12 +20,15 @@ player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 
 player_rect = player.get_rect()
-player_speed = [1, 1]
-playing = True 
+player_move_down = [0, 1]
+player_move_right = [1, 0]
+player_move_up = [0, -1]
+player_move_left = [-1, 0]
 
+playing = True 
 while playing:
 
-    FPS.tick(220)
+    FPS.tick(420)
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -31,21 +36,19 @@ while playing:
 
     main_display.fill(COLOR_BLACH)  
 
-    if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
+    keys = pygame.key.get_pressed()
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_up)
+    if keys[K_LEFT] and player_rect.left > 0:
+        player_rect = player_rect.move(player_move_left)    
 
-    if player_rect.top < 0:
-        player_speed[1] = -player_speed[1] 
-
-    if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
-
-    if player_rect.left < 0:
-        player_speed[0] = -player_speed[0]
 
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_speed)
 
     pygame.display.flip()
    
