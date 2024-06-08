bff1916c10e5279ba4f5da76e6c4f734e421e321
Tais Kotliar <kryos.eva@gmail.com>
2024-06-08 14:08:21 +0300

    add enemies

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ FPS = pygame.time.Clock()
 
 HEIGHT = 800
 WIDTH = 1200
-COLOR_WHITE = (255, 255, 255)
-COLOR_BLACH = (0, 0, 0)
+COLOR_PLAYER = (245, 118, 181)
+COLOR_BACHGROUND = (33, 12, 22)
+COLOR_ENEMY = (118, 148, 245)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 player_size = (20, 20)
 
 player = pygame.Surface(player_size)
 
-player.fill(COLOR_WHITE)
+player.fill(COLOR_PLAYER)
 
 player_rect = player.get_rect()
 player_move_down = [0, 1]
@@ -25,16 +26,34 @@ player_move_right = [1, 0]
 player_move_up = [0, -1]
 player_move_left = [-1, 0]
 
+
+def create_enemy():
+    enemy_size = (30, 30)
+    enemy = pygame.Surface(enemy_size)
+    enemy.fill(COLOR_ENEMY)
+    enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT - 30), *enemy_size)
+    enemy_move = [random.randint(-6, 1), 0]
+    return [enemy, enemy_rect, enemy_move]
+
+
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
+
 playing = True 
 while playing:
 
-    FPS.tick(420)
+    FPS.tick(120)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
 
-    main_display.fill(COLOR_BLACH)  
+    main_display.fill(COLOR_BACHGROUND)   
 
     keys = pygame.key.get_pressed()
     if keys[K_DOWN] and player_rect.bottom < HEIGHT:
@@ -46,9 +65,16 @@ while playing:
     if keys[K_LEFT] and player_rect.left > 0:
         player_rect = player_rect.move(player_move_left)    
 
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(enemy[2])
+        main_display.blit(enemy[0], enemy[1])
 
+
+    #enemy_rect = enemy_rect.move(enemy_move)
     main_display.blit(player, player_rect)
 
+    #main_display.blit(enemy, enemy_rect)
 
+    print(len(enemies))
     pygame.display.flip()
    
